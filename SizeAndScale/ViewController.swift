a9a68fb2c504b12e2fdc8f29d9fadaf34c722bcb
alphaZ <ioszhangdong@163.com>
2018-12-13 11:37:13 +0800

    Initial Commit

---
+++ SizeAndScale/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SizeAndScale
+//
+//  Created by AlphaZ on 2018/12/13.
+//  Copyright © 2018年 Zhang,Dong(AI). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
